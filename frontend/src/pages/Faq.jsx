16c8624e6e22695685fce45cc5fe7aa6f3331d48
Ayush Bhadauriya <bhadhauriyaayush@gmail.com>
2026-09-17 14:06:26 +0530

    Fix FAQ CSS filename case

--- frontend/src/pages/Faq.jsx
+++ frontend/src/pages/Faq.jsx
@@ -9,7 +9,7 @@ import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-import "./FAQ.css";
+import "./Faq.css";
 
 const faqData = [
   {
